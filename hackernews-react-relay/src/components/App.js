2b7e6a21dc1e515f5251f97665f66f31109eaa07
Alex Trzeciak <trebek08@gmail.com>
2018-05-12 23:43:24 -0700

    implement react router and redirect

--- hackernews-react-relay/src/components/App.js
+++ hackernews-react-relay/src/components/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import LinkListPage from './LinkListPage'
 import CreateLink from './CreateLink'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
-    return (
-      <LinkListPage />
-    )
-  }
+	  return (
+	    <div className='center w85'>
+	      <Header />
+	      <div className='ph3 pv1 background-gray'>
+	        <Switch>
+	          <Route exact path='/' component={LinkListPage}/>
+	          <Route exact path='/create' component={CreateLink}/>
+	         </Switch>
+	      </div>
+	    </div>
+	  )
+	}
 }
 
 export default App
